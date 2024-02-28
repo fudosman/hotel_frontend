d6d34dd9767849ae8947b2993579001d5bc68048
fudosman <fudosman@gmail.com>
2024-02-28 14:13:39 +0100

    Initial commit

---
+++ src/pages/Register.tsx
@@ -0,0 +1,89 @@
+import { useForm } from "react-hook-form";
+import { useMutation } from "react-query";
+import * as apiClient from "../api-client"
+
+export type RegisterFormData = {
+  firstname: string;
+  lastname: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+}
+
+const Register = () => {
+  const { register, watch, handleSubmit, formState: { errors } } = useForm<RegisterFormData>();
+  const mutation = useMutation(apiClient.register, {
+    onSuccess: () => {
+      console.log('registeration successful');
+     },
+    onError: (error: Error) => {
+      console.log(`Registration Failed: ${error.message}`);
+     }
+  });
+
+  const onSubmit = handleSubmit((data) => {
+    mutation.mutate(data);
+    });
+
+  return (
+    <form className="flex flex-col gap-5" onSubmit={onSubmit}>
+      <h2 className="font-bold text-3xl">Create an Account</h2>
+      <div className="flex flex-col md:flex-row gap-5">
+        <label className="text-grey-700 text-sm font-bold flex-1">
+          First Name
+          <input className="border rounded w-full py-1 px-2 font-normal" {...register("firstname", { required: "firstname field is required" })}></input>
+          {errors.firstname && (
+            <span className="text-red-500">{ errors.firstname.message}</span>
+          )}
+        </label>
+        <label className="text-grey-700 text-sm font-bold flex-1">
+          Last Name
+          <input className="border rounded w-full py-1 px-2 font-normal" {...register("lastname", { required: "lastname field is required" })}></input>
+           {errors.lastname && (
+            <span className="text-red-500">{ errors.lastname.message}</span>
+          )}
+        </label>
+      </div>
+      <label className="text-grey-700 text-sm font-bold flex-1">
+          Email
+        <input type="email" className="border rounded w-full py-1 px-2 font-normal" {...register("email", { required: "Email field is required" })}></input>
+         {errors.email && (
+            <span className="text-red-500">{ errors.email.message}</span>
+          )}
+      </label>
+       <label className="text-grey-700 text-sm font-bold flex-1">
+          Password
+        <input type="password" className="border rounded w-full py-1 px-2 font-normal" {...register("password", {
+          required: "Password field is required",
+          minLength: {
+            value: 6,
+            message: "Password must be at least 6 characters",
+          }
+        })}></input>
+         {errors.password && (
+            <span className="text-red-500">{ errors.password.message}</span>
+          )}
+      </label>
+      <label className="text-grey-700 text-sm font-bold flex-1">
+         Confirm Password
+        <input type="password" className="border rounded w-full py-1 px-2 font-normal" {...register("confirmPassword", {
+          validate: (value) => {
+            if (!value) {
+              return "this field is required"
+            } else if (watch("password") !== value) {
+              return "your password do not match"
+            }
+          }
+        })}></input>
+         {errors.confirmPassword && (
+            <span className="text-red-500">{ errors.confirmPassword.message}</span>
+          )}
+      </label>
+      <span>
+        <button type="submit" className="bg-blue-600 text-white p-2 font-bold hover:bg-blue-500 text-xl">Create Account</button>
+      </span>
+    </form>
+  )
+}
+
+export default Register;
